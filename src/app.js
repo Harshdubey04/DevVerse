fa3202c197645358f18ea469beaff0aa5272c580
Harsh Dubey <harshdu001@gmail.com>
2026-07-27 20:32:51 +0530

    First commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express=require("express");
+
+const app=express();
+
+app.get("/",(req,res)=>{
+    res.send("Server running at port 3000");
+})
+
+app.listen(3000,(req,res)=>{
+    console.log("Server running at port 3000");
+})

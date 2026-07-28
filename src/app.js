178e34837e040448c3276932130c7f05bbcc94b4
Harsh Dubey <harshdu001@gmail.com>
2026-07-28 20:31:03 +0530

    Database connected using mongoose

--- src/app.js
+++ src/app.js
@@ -1,23 +1,18 @@
 const express=require("express");
 
 const app=express();
-const {adminAuth,userAuth}=require("./middleware/auth");
 
-app.use("/admin",adminAuth);
-app.use("/user",userAuth);
+const {connectDB}=require("./config/database");
 
-app.get("/",(req,res)=>{
-    res.send("Home route");
-})
-
-app.get("/admin/getAllData",(req,res)=>{
-    res.status(201).send("All admin data sent...");
-})
-
-app.get("/user/getAllData",(req,res)=>{
-    res.send("User data sent");
-})
-
-app.listen(3000,(req,res)=>{
+connectDB()
+.then(()=>{
+    console.log("Connected to DB successfully");
+    app.listen(3000,(req,res)=>{
     console.log("Server running at port 3000");
 })
+})
+.catch(err=>{
+    console.log(err)
+})
+
+
---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+
+const mongooose=require("mongoose");
+
+const connectDB=async()=>{
+    await mongooose.connect(
+        "mongodb+srv://harshdu001_db_user:mDHZxyH7MKuA1toO@cluster1.z7pm4de.mongodb.net/DevVerse"
+    )
+}
+
+
+module.exports={
+    connectDB
+}
